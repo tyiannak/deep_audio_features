277b2c50fef55ad0c6fdfc12fbfd79a584400c9f
Dimitris <dkatsiros@gmail.com>
2020-07-24 16:34:53 +0300

    Add sound processing utils

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 # Sampling settings
 SAMPLING_RATE = 16000
-WIN_SIZE = round(50 * 1e-3 * SAMPLING_RATE)  # 50 msec
+WINDOW_LENGTH = round(50 * 1e-3 * SAMPLING_RATE)  # 50 msec
 HOP_LENGTH = round(50 * 1e-3 * SAMPLING_RATE)  # 50 msec -- no overlapping
