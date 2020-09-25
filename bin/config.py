c311217afe3359ea3163863b37e94970ed2aaa14
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-09-25 17:39:45 +0300

    bug

--- bin/config.py
+++ bin/config.py
@@ -7,8 +7,8 @@ CNN_BOOLEAN = True
 # .pkl files
 VARIABLES_FOLDER = "pkl/"
 # Sampling settings
-WINDOW_LENGTH = round(50 * 1e-3)  # 50 msec
-HOP_LENGTH = round(10 * 1e-3 )    # 10 msec step
+WINDOW_LENGTH = (50 * 1e-3)  # 50 msec
+HOP_LENGTH = (10 * 1e-3 )    # 10 msec step
 
 # Dataloader
 BATCH_SIZE = 16
