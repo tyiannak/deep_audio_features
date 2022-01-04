ddf02f4c14d17cb63857b4f7cee8c62bb683742c
Theodoros Giannakopoulos <tyiannak@gmail.com>
2022-01-04 20:01:07 +0200

    add optional filtering in durations before plotting

--- deep_audio_features/bin/tool_plot_durations.py
+++ deep_audio_features/bin/tool_plot_durations.py
@@ -1,10 +1,11 @@
 import contextlib
 import wave
+from numpy.lib import percentile
 import plotly.express as px
 import os
 import pandas as pd
 import argparse
-
+import numpy as np
 
 def get_wav_duration(fname):
     with contextlib.closing(wave.open(fname,'r')) as f:
@@ -26,20 +27,31 @@ def getListOfFiles(dirName):
 
     return allFiles
 
-def plot_hist(dir):
+
+def plot_hist(dir, low, high):
 
     file_list = getListOfFiles(dir)
     durations = []
     print('--> Calculating durations')
     for file in file_list:
-        durations.append(get_wav_duration(file))
+        d = get_wav_duration(file)
+        if (d >= low) and (d <= high):
+            durations.append(d)
     df = pd.DataFrame({'duration': durations})
+    p5 = np.percentile(durations, 5.0)
+    p95 = np.percentile(durations, 95.0)
+    m = np.mean(durations)
+    percentage = 100 * len(durations) / float(len(file_list))
+    print('Statistics:')
+    print(f'mu = {m:.2f} [{p5:.2f}, {p95:.2f}]')
+    print(f'Percentage of data used: {percentage:.1f}%')
     print('--> Plotting histogram of audio durations')
     fig = px.histogram(df, x="duration")
     fig.update_layout(title_text='Audio durations in s', title_x=0.5)
     fig.show()
 
     fig.write_html(dir + "_durations.html")
+
 
 if __name__ == '__main__':
 
@@ -50,5 +62,17 @@ if __name__ == '__main__':
                         required=True,
                         default=None,
                         help='Input directory')
+    parser.add_argument('-l',
+                        '--low',
+                        type=float,
+                        required=False,
+                        default=0,
+                        help='Lowest duration to be used')
+    parser.add_argument('-u',
+                        '--upper',
+                        type=float,
+                        required=False,
+                        default=10000,
+                        help='Highest duration to be used')
     FLAGS = parser.parse_args()
-    plot_hist(FLAGS.input)
+    plot_hist(FLAGS.input, FLAGS.low, FLAGS.upper)
